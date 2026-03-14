8a2d88268215f7b92c9fbe8e0eca28c2332b3d23
Marlon Progetti <marlop.dossantos@gmail.com>
2026-03-14 19:44:02 -0300

    Adicionando aqrquivo database.sql com a estrutura do banco usada.

---
+++ database.sql
@@ -0,0 +1,20 @@
+CREATE DATABASE helpdesk_tcc;
+USE helpdesk_tcc;
+
+CREATE TABLE usuarios (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    perfil VARCHAR(20) DEFAULT 'usuario'
+);
+
+CREATE TABLE chamados (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    titulo VARCHAR(150) NOT NULL,
+    descricao TEXT NOT NULL,
+    categoria VARCHAR(50) NOT NULL,
+    status VARCHAR(30) DEFAULT 'Aberto',
+    data_abertura DATETIME DEFAULT CURRENT_TIMESTAMP,
+    usuario_id INT,
+    FOREIGN KEY (usuario_id) REFERENCES usuarios(id)
+);
